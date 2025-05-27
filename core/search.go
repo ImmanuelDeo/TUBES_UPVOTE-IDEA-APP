794248d42607e216c907eab2e6fae6fa604fdfec
ImmanuelDeo <deoimmanuel59@gmail.com>
2025-05-27 07:28:21 +0700

    delete binary search func

--- core/search.go
+++ core/search.go
@@ -10,19 +10,3 @@ func SequentialSearch(arr [model.MaxIdeas]model.Idea, count int, keyword string)
 	}
 	return -1
 }
-
-func BinarySearch(arr [model.MaxIdeas]model.Idea, count int, keyword string) int {
-	low := 0
-	high := count - 1
-	for low <= high {
-		mid := (low + high) / 2
-		if arr[mid].Title == keyword {
-			return mid
-		} else if arr[mid].Title < keyword {
-			low = mid + 1
-		} else {
-			high = mid - 1
-		}
-	}
-	return -1
-}
